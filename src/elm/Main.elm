c932065908d1db50bc1e71d941cbc5a0a38f7efa
Ernst Bunders <Ernst.Bunders@topicus.nl>
2018-01-21 11:55:25 +0100

    taken met test data

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (checked, class, type_)
 import Html.Events exposing (onCheck, onClick)
 import Navigation exposing (..)
 import UrlParser exposing (oneOf)
@@ -21,7 +21,20 @@ main =
 
 
 type alias Model =
-    { page : Page }
+    { page : Page
+    , tasks : List Task
+    }
+
+
+type alias TaskId =
+    Int
+
+
+type alias Task =
+    { id : TaskId
+    , title : String
+    , done : Bool
+    }
 
 
 type Page
@@ -35,9 +48,16 @@ type Page
 
 init : Location -> ( Model, Cmd Msg )
 init location =
-    ( { page = (locationToPage location) }
-    , Cmd.none
-    )
+    let
+        tasks =
+            [ Task 1 "task 1" False
+            , Task 2 "task 2" False
+            , Task 3 "task 3" True
+            ]
+    in
+        ( Model (locationToPage location) tasks
+        , Cmd.none
+        )
 
 
 
@@ -56,6 +76,8 @@ route =
 type Msg
     = NewUrl String
     | UrlChange Navigation.Location
+    | ToggleDone Int
+    | RemoveDone
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -72,6 +94,27 @@ update msg model =
             ( { model | page = (locationToPage location) }
             , Cmd.none
             )
+
+        ToggleDone taskId ->
+            let
+                newTasks =
+                    List.map
+                        (\task ->
+                            if task.id == taskId then
+                                { task | done = not task.done }
+                            else
+                                task
+                        )
+                        model.tasks
+            in
+                ( { model | tasks = newTasks }, Cmd.none )
+
+        RemoveDone ->
+            let
+                newTasks =
+                    List.filter (\task -> not task.done) model.tasks
+            in
+                ( { model | tasks = newTasks }, Cmd.none )
 
 
 locationToPage : Location -> Page
@@ -111,25 +154,55 @@ view model =
         div [ class "container" ]
             [ div [ class "row" ]
                 [ div [ class "col-6 mx-auto" ]
-                    [ viewNavigation
-                    , page
+                    [ page
                     ]
                 ]
             ]
 
 
-viewNavigation : Html Msg
-viewNavigation =
+viewTasksPage : Model -> Html Msg
+viewTasksPage model =
     div []
-        [ a [ onClick (NewUrl "#tasks") ] [ text "Tasks page" ]
-        , span [] [ text " - " ]
-        , a [ onClick (NewUrl "#edit") ] [ text "Edit page" ]
+        [ h1 [] [ text "Tasks Page" ]
+        , (viewTasks model.tasks)
+        , viewButtons
         ]
 
 
-viewTasksPage : Model -> Html Msg
-viewTasksPage model =
-    h1 [] [ text "Tasks Page" ]
+viewButtons : Html Msg
+viewButtons =
+    div [ class "btn-group float-right mt-3" ]
+        [ button [ class "btn btn-outline-secondary", type_ "button", onClick RemoveDone ] [ text "Remove done" ]
+        , button [ class "btn btn-outline-primary", type_ "button" ] [ text "Add Todo" ]
+        ]
+
+
+viewTasks : List Task -> Html Msg
+viewTasks tasks =
+    ul [ class "list-group mt-3" ]
+        (List.map viewTask tasks)
+
+
+viewTask : Task -> Html Msg
+viewTask task =
+    let
+        taskTitle =
+            case task.done of
+                True ->
+                    del [] [ text task.title ]
+
+                False ->
+                    text task.title
+    in
+        li [ class "list-group-item" ]
+            [ div []
+                [ label []
+                    [ input [ onClick (ToggleDone task.id), type_ "checkbox", checked task.done ] []
+                    , span [] [ taskTitle ]
+                    ]
+                , a [ class "float-right" ] [ text "Edit" ]
+                ]
+            ]
 
 
 viewEditTaskPage : Model -> Html msg
